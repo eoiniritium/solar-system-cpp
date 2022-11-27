f4d74439a7e441a93cba5bc674cb279f5664a689
Eoin <mazecraze06@gmail.com>
2022-11-27 22:19:38 +0000

    Add Body dialog additions!

--- include/gui.hpp
+++ include/gui.hpp
@@ -215,9 +215,12 @@ class Button {
 class Entry {
     private:
     std::string *text;
+    char* buf; // EW
+    int index;
     Color colour;
     int x, y, width, height, maxLength;
     double fontsize;
+    bool focused;
     public:
     Entry(std::string &stringvar, int maxLength, int x, int y, int width, int height, double fontsize, Color colour) {
         this->text = &stringvar;
@@ -228,13 +231,49 @@ class Entry {
         this->height = height;
         this->colour = colour;
         this->fontsize = fontsize;
+        this->buf = new char[maxLength];
+        this->index = 0;
+        this->focused = false;
     }
 
     void draw() {
         DrawRectangle(x, y, width, height, colour);
-        DrawRectangle(x+2, y+2, width-4, height-4, BLACK);
+        
+
+        if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+            Vector2 mp = GetMousePosition();
+            int mx = mp.x;
+            int my = mp.y;
+
+            if(mx >= x && mx <= x + width && my >= y && my <= y + height) {
+                focused = true;
+                
+            } else {
+                focused = false;
+            }
+        }
+
+        if(focused) {
+            DrawRectangle(x+2, y+2, width-4, height-4, GRAY);
+        }
+        else {
+            DrawRectangle(x+2, y+2, width-4, height-4, BLACK);
+        }
 
         // Text handling
+        int key = GetKeyPressed();
+        if(key && focused) {
+            if(key == KEY_BACKSPACE)
+            { // Capture all backspace, otherwise _ will be added for some reason
+                if(index > 0)
+                    buf[--index] = 0;
+            }
+            else if(index < maxLength)
+                buf[index++] = key;
+            *text = buf;
+        }
+
+        DrawText(buf, this->x + 5, this->y + (height - fontsize)/2, fontsize, WHITE); // maybe just text?
     }
 };
 
@@ -255,32 +294,44 @@ class AddBodyDialog {
     double scale;
     DialogState state;
     std::vector<Body> *bodies; // Pointer to body vector
-    bool *flag;
+    bool *dialogFlag;
+    bool chooseLocationFlag;
 
     Entry *nameEntry;
+    Button *chooseLocationButton;
 
-    M bodyRealX, bodyVirtualX;
-    M bodyRealY, bodyVirtualY;
+    M bodyVirtualX;
+    M bodyVirtualY;
     MS_1 bodyUVX, bodyUVY;
     KG bodyMass;
     std::string bodyname;
-    
+
+    // For this only
+    std::string locationString;
+
     public:
-    AddBodyDialog(std::vector<Body> &bodiesVector, double scale, bool &flag, int screenWidth, int screenHeight) {
+    AddBodyDialog(std::vector<Body> &bodiesVector, double scale, bool &dialogFlag, int screenWidth, int screenHeight) {
         this->screenWidth = screenWidth;
         this->screenHeight = screenHeight;
         this->width = 500;
         this->height = 600;
         this->borderThickness = 2;
+        this->scale = scale;
 
         this->bodies = &bodiesVector; // Passed by referance
-        this->flag = &flag; // Address of flag
+        this->dialogFlag = &dialogFlag; // Should this be open?
 
         this->diagX = (screenWidth - width)/2 + borderThickness;
         this->diagY = (screenHeight-height)/2 + borderThickness; 
         this->state = DialogState::AddBody;
 
-        this->nameEntry = new Entry(bodyname, 10, diagX + 5, diagY + 70, 300, 30, 16.0f, WHITE);
+        this->nameEntry = new Entry(bodyname, 20, diagX + 5, diagY + 70, 300, 30, 16.0f, WHITE);
+        this->chooseLocationButton = new Button("Choose Location", chooseLocationFlag, diagX + 5, diagY + 110, 200, 50, WHITE, 16.0f);
+
+        this->bodyVirtualX = 0;
+        this->bodyVirtualY = 0;
+
+        this->locationString = "X: 0 Y: 0";
     }
 
     ~AddBodyDialog() {
@@ -291,6 +342,13 @@ class AddBodyDialog {
         Vector2 mousepos = GetMousePosition();
         double mx = mousepos.x;
         double my = mousepos.y;
+
+        
+
+        if(chooseLocationFlag) {
+            state = DialogState::ChooseLocation;
+        }
+
         switch(state) {
             case AddBody:
                 // Dialog box
@@ -302,19 +360,52 @@ class AddBodyDialog {
 
                 // X button
                 DrawRectangle(diagX + width - 50, diagY + 10, 30, 30, WHITE);
-                DrawRectangle(diagX + width - 48, diagY + 12, 26, 26, BLACK);
-                DrawText("X", diagX + width - 45, diagY + 13, 18.0f, WHITE);
-                if(mx >= diagX + width - 50 && mx <= diagX + width - 20 && my >= diagY + 10 && my <= diagY + 40 && IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-                    *flag = false; // Exits dialog
+                if(mx >= diagX + width - 50 && mx <= diagX + width - 20 && my >= diagY + 10 && my <= diagY + 40) {
+                    DrawRectangle(diagX + width - 48, diagY + 12, 26, 26, GRAY);
+                    if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
+                        resetAllVariables();
+                        *dialogFlag = false; // Exits dialog
+                    }
                 }
-
+                else {
+                    DrawRectangle(diagX + width - 48, diagY + 12, 26, 26, BLACK);
+                }
+                DrawText("X", diagX + width - 45, diagY + 13, 18.0f, WHITE);
+                
                 // Body name
                 DrawText("Body Name:", diagX + 5, diagY + 50, 16.0f, WHITE);
                 nameEntry->draw();
+                
+                // Body location
+                chooseLocationButton->draw(mousepos);
+                DrawText(locationString.c_str(), diagX + 220, diagY + 127, 16.0f, WHITE);
 
                 break;
             case ChooseLocation:
+                DrawLine(mx, 0, mx, screenHeight, WHITE);
+                DrawLine(0, my, screenWidth, my, WHITE);
+                DrawText("Click to select location", mx + 10, my + 10, 16.0f, WHITE);
+
+                if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+                    bodyVirtualX = mx * scale;
+                    bodyVirtualY = my * scale;
+                    locationString = "X: " + removeTrailingCharacters(std::to_string(bodyVirtualX), '0') + "0m Y: " + removeTrailingCharacters(std::to_string(bodyVirtualY), '0') + "0m";
+                    state = AddBody;
+                    chooseLocationFlag = false;
+                }
                 break;
         } 
     }
+
+    void resetAllVariables() {
+        bodyVirtualX = 0;
+        bodyVirtualY = 0;
+        bodyUVX = 0;
+        bodyUVY = 0;
+        bodyMass = 0;
+        bodyname = "";
+
+        delete nameEntry; // No memory leak
+        this->nameEntry = new Entry(bodyname, 20, diagX + 5, diagY + 70, 300, 30, 16.0f, WHITE);
+    };
 };
